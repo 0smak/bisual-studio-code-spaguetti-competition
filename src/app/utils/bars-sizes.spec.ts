d3676ca379cc6c2ef27486b8b89292debe254425
Óscar Muñoz <osmak@protonmail.com>
2020-12-19 14:28:30 +0100

    cleanup

--- src/app/utils/bars-sizes.spec.ts
+++
@@ -1,7 +0,0 @@
-import { BarsSizes } from './bars-sizes';
-
-describe('BarsSizes', () => {
-  it('should create an instance', () => {
-    expect(new BarsSizes()).toBeTruthy();
-  });
-});
--- src/app/utils/dir-tree.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DirTree } from './dir-tree';
-
-describe('DirTree', () => {
-  it('should create an instance', () => {
-    expect(new DirTree()).toBeTruthy();
-  });
-});
